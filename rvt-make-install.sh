0bb77bf828d361a56d5d5e36983cbd249cd83138
Rijnard <rvantonder@gmail.com>
2016-05-31 03:45:28 +0000

    auto script

---
+++ rvt-make-install.sh
@@ -0,0 +1,10 @@
+./configure --prefix=/home/vagrant/pfff-cust 
+make
+make opt
+make install
+
+# copy the scheck test requirements
+cp -r ~/pfff/tests/php/scheck/* ~/pfff-cust/share/pfff/tests/php/scheck
+
+cd ~/pfff-cust
+bin/scheck -verbose -test 
