972e46e615cabddecdc3acda1edabae19204df8c
Rijnard <rvantonder@gmail.com>
2016-06-20 00:12:34 -0400

    yes.

--- lang_php/analyze/checker/check_micro_clones_php.ml
+++ lang_php/analyze/checker/check_micro_clones_php.ml
@@ -7,116 +7,82 @@ open Printf
 let (!) = Export_ast_php.ml_pattern_string_of_expr
 let str_of_tok tok = Parse_info.str_of_info tok
 
-module Bexp : sig
+module Boolean : sig
+  type op = And | Or
 
-  type symbol = Ast_php.expr * Ast_php.tok option (* parent tok *)
-  type t =
-    | Var of symbol
-    | And of t list
-    | Or  of t list
-    | L_and of t list
-    | L_or of t list
-    | LL_and of t list
-    | LL_or of t list
+  type 'a t = private
+    | Atom of 'a
+    | List of op * 'a t list
 
-  val map_children : (t list -> t list) -> t -> t
+  val make : op -> 'a t -> 'a t -> 'a t
 
-  val apply_children : (t list -> 'b option) -> t -> 'b option
-
-  val to_string : ?z3:bool -> t -> string
+  module Lang : sig
+    val (||) : 'a t -> 'a t -> 'a t
+    val (&&) : 'a t -> 'a t -> 'a t
+    val v : 'a -> 'a t
+  end
 end = struct
-  type symbol = Ast_php.expr * Ast_php.tok option
-  type t =
-    | Var of symbol
-    | And of t list
-    | Or  of t list
-    | L_and of t list
-    | L_or of t list
-    | LL_and of t list
-    | LL_or of t list
+  type op = And | Or
+  type 'a t =
+    | Atom of 'a
+    | List of op * 'a t list
 
+  let make op x y =
+    match x,y with
+    | List (op1,xs), List (op2,ys) when op1 = op && op2 = op ->
+      List (op, xs@ys)
+    | _,List (op2,ys) when op2 = op ->
+      List (op,x::ys)
+    | List (op1,xs),_ when op1 = op ->
+      List (op,xs@[y])
+    | x,y ->
+      List (op,[x;y])
 
-  let map_children (f: t list -> t list) (e : t) : t =
-    match e with
-    | And exps -> And (f exps)
-    | Or exps -> Or (f exps)
-    | L_or exps -> L_or (f exps)
-    | L_and exps -> L_and (f exps)
-    | LL_or exps -> LL_or (f exps)
-    | LL_and exps -> LL_and (f exps)
-    | x -> x
-
-  let apply_children (f: t list -> 'b option) (e : t) : 'b option =
-    match e with
-    | And exps -> f exps
-    | Or exps -> f exps
-    | L_or exps -> f exps
-    | L_and exps -> f exps
-    | LL_or exps -> f exps
-    | LL_and exps -> f exps
-    | x -> None
-
-  let to_string ?(z3=false) e =
-    let open Printf in
-    let rec to_string_exp =
-      function
-      | Or x -> sprintf "Or(%s)" @@ list_to_string x
-      | And x -> sprintf "And(%s)" @@ list_to_string x
-      | L_or x -> sprintf "Land(%s)" @@ list_to_string x
-      | L_and x -> sprintf "Lor(%s)" @@ list_to_string x
-      | LL_or x -> sprintf "LLor(%s)" @@ list_to_string x
-      | LL_and x -> sprintf "LLand(%s)" @@ list_to_string x
-      | Var (e,_) ->
-        (match e with
-         | Ast_php.IdVar (Ast_php.DName(v,_),_) ->
-           if z3 then sprintf "B('%s')" v
-           else sprintf "%s" v
-         | x -> sprintf "%s" !x)
-    and list_to_string (l : t list) : string =
-      List.fold_left (fun (c,acc) x ->
-          match c with
-          | 0 -> (c+1),(to_string_exp x)
-          | _ -> (c+1),(acc^", "^(to_string_exp x))) (0,"") l
-      |> snd in
-    to_string_exp e
+  module Lang = struct
+    let (||) op1 op2 = make Or op1 op2
+    let (&&) op1 op2 = make And op1 op2
+    let v x = Atom x
+  end
 end
 
-let bool_exp_of_php_exp exp =
+type s = Ast_php.expr * Ast_php.tok option
+
+open Boolean
+
+let op_to_string = function
+  | And -> "And"
+  | Or -> "Or"
+
+let to_string exp =
+  let (!) = Export_ast_php.ml_pattern_string_of_expr in
+  let rec exp_to_string =
+    function
+    | Atom (Ast_php.IdVar (Ast_php.DName(v,_),_),_) -> sprintf "%s" v
+    | Atom (x,_) -> !x
+    | List (op,l)
+      -> sprintf "%s(%s)" (op_to_string op) (list_to_string l)
+  and
+    list_to_string (l : s t list) : string =
+    List.fold_left (fun (c,acc) x ->
+        match c with
+        | 0 -> (c+1),(exp_to_string x)
+        | _ -> (c+1),(acc^", "^(exp_to_string x))) (0,"") l |> snd
+  in
+  exp_to_string exp
+
+(** create a Boolean.t expression from an AST expression *)
+let bool_exp_of_php_exp exp : s Boolean.t =
   let open Printf in
   let open Ast in
-  let rec aux exp parent_tok =
+  let rec aux exp parent_tok : s Boolean.t =
     match exp with
     | Binary (lhs,(Logical OrBool,op_tok),rhs) ->
-      Bexp.Or [aux lhs (Some op_tok); aux rhs (Some op_tok)]
+      Boolean.Lang.(aux lhs (Some op_tok) || aux rhs (Some op_tok))
     | Binary (lhs,(Logical AndBool,op_tok),rhs) ->
-      Bexp.And [aux lhs (Some op_tok); aux rhs (Some op_tok)]
-    | Binary (lhs,(Logical AndLog,op_tok),rhs) ->
-      Bexp.L_and [aux lhs (Some op_tok); aux rhs (Some op_tok)]
-    | Binary (lhs,(Logical OrLog,op_tok),rhs) ->
-      Bexp.L_or [aux lhs (Some op_tok); aux rhs (Some op_tok)]
-    | Binary (lhs,(Arith And,op_tok),rhs) ->
-      Bexp.LL_and [aux lhs (Some op_tok); aux rhs (Some op_tok)]
-    | Binary (lhs,(Arith Or,op_tok),rhs) ->
-      Bexp.LL_or [aux lhs (Some op_tok); aux rhs (Some op_tok)]
-    | ParenExpr (_,exp,_) as x ->
-      (match parent_tok with
-       | Some tok when str_of_tok tok = "||" ->
-         Bexp.Or [aux exp parent_tok]
-       | Some tok when str_of_tok tok = "&&" ->
-         Bexp.And [aux exp parent_tok]
-       | Some tok when str_of_tok tok = "and" ->
-         Bexp.L_and [aux exp parent_tok]
-       | Some tok when str_of_tok tok = "or" ->
-         Bexp.L_or [aux exp parent_tok]
-       | Some tok when str_of_tok tok = "|" ->
-         Bexp.LL_or [aux exp parent_tok]
-       | Some tok when str_of_tok tok = "&" ->
-         Bexp.LL_and [aux exp parent_tok]
-       | _ -> Bexp.Var (x,parent_tok))
-    | x -> Bexp.Var (x,parent_tok)
+      Boolean.Lang.(aux lhs (Some op_tok) && aux rhs (Some op_tok))
+    | ParenExpr (_,exp,_) -> aux exp parent_tok
+    | x -> Boolean.Lang.v (x, parent_tok)
   in aux exp None
-
-open Bexp
 
 let err_msg_of_tok tok =
   Parse_info.token_location_of_info tok
@@ -126,50 +92,12 @@ let err_msg_of_tok tok =
     info.Parse_info.line
     info.Parse_info.column
 
-let flatten exp =
-  let rec aux (exp : Bexp.t) : Bexp.t =
-    match exp with
-    | Or l -> flatten_exp l (Or [])
-    | And l -> flatten_exp l (And [])
-    | L_or l -> flatten_exp l (L_or [])
-    | L_and l -> flatten_exp l (L_and [])
-    | LL_or l -> flatten_exp l (LL_or [])
-    | LL_and l -> flatten_exp l (LL_and [])
-    | x -> x
-  and
-    flatten_exp l init =
-    List.fold_right (fun x acc ->
-        let res = aux x in
-        match acc,res with
-        | And l,And t -> And (t@l)
-        | And l, x -> And (x::l)
-        | Or l, Or t -> Or (t@l)
-        | Or l, x -> Or (x::l)
-        | L_or l, L_or t -> L_or (t@l)
-        | L_or l, x -> L_or (x::l)
-        | L_and l, L_and t -> L_and (t@l)
-        | L_and l, x -> L_and (x::l)
-        | LL_or l, LL_or t -> LL_or (t@l)
-        | LL_or l, x -> LL_or (x::l)
-        | LL_and l, LL_and t -> LL_and (t@l)
-        | LL_and l, x -> LL_and (x::l)
-        | _,x -> x) l init
-  in aux exp
-
-let print_rewrite rule exp exp' =
-  printf "\t[+] Rule: %s %s -> %s\n" rule (to_string exp) (to_string exp')
-
 (** Find the first var and use that. [v] is for the actual statement *)
 let print_error ?(v=false) exp =
   let res  =
     let rec aux = function
-      | Var x -> Some x
-      | And (hd::_) -> aux hd
-      | Or (hd::_) -> aux hd
-      | L_and (hd::_) -> aux hd
-      | L_or (hd::_) -> aux hd
-      | LL_and (hd::_) -> aux hd
-      | LL_or (hd::_) -> aux hd
+      | Atom x -> Some x
+      | List (_,hd::_) -> aux hd
       | _ -> None
     in aux exp in
   match res with
@@ -181,81 +109,45 @@ let print_error ?(v=false) exp =
   | _ -> ()
 
 let compare exp1 exp2 =
-  String.compare (Bexp.to_string exp1) (Bexp.to_string exp2)
+  String.compare (to_string exp1) (to_string exp2)
 
-(**
-   Or([Var "a"; Var "b"; Var "a"]) -> Or([Var "a"; Var "b"])
-*)
-let rule_dedup ?(v=true) exp =
-  let (!) = to_string in
-  let dedup l =
-    List.fold_right (fun x acc ->
-        if List.exists (fun y -> !x = !y) acc
-        then (print_error x;
-              acc) else x::acc) l [] in
-  Bexp.map_children dedup exp
+let dedup l =
+  List.fold_left (fun acc x ->
+      if List.exists (fun y -> compare x y = 0) acc
+      then (print_error x; acc) else x::acc) [] l |> List.rev
 
-(**
-   Unbox if there is only one expression in the list of a bool exp
-   Or([And([Var "b";Var "c"])]) -> And([Var "b"; Var "c"])
-   Or([Var x]) -> Var x
+let boolean_of_list op (l : s Boolean.t list) : s Boolean.t =
+  let rec aux l =
+    match l with
+    | [Atom x] -> Lang.v x
+    | x::y::[] -> make op x y
+    | hd::tl  -> make op hd (aux tl)
+    | [] -> failwith "Error: empty list. Invariant broken."
+  in aux l
 
-   Bexp.apply_children (fun l ->
-      match l with
-      | (x::[]) -> Some x
-      | x -> None) exp
-   |> function
-   | Some res -> res
-   | None -> exp
-*)
-let rule_simple ?(v=false) =
-  function
-  | Or (x::[]) -> x
-  | And (x::[]) -> x
-  | L_or (x::[]) -> x
-  | L_and (x::[]) -> x
-  | LL_or (x::[]) -> x
-  | LL_and (x::[]) -> x
-  | x -> x
+let rule_dedup (exp : s Boolean.t) : s Boolean.t =
+  match exp with
+  | List (op,l) -> dedup l |> boolean_of_list op
+  | Atom x -> Lang.v x
 
-let rewrite exp =
+let bur_map f (exp : s Boolean.t) : s Boolean.t =
   let rec aux exp =
     match exp with
-    | And x ->
-      let res = List.map aux x in
-      And res |> rule_dedup |> rule_simple
-    | Or x ->
-      let res = List.map aux x in
-      Or res |> rule_dedup |> rule_simple
-    | L_or x ->
-      let res = List.map aux x in
-      L_or res |> rule_dedup |> rule_simple
-    | L_and x ->
-      let res = List.map aux x in
-      L_and res |> rule_dedup |> rule_simple
-    | LL_or x ->
-      let res = List.map aux x in
-      LL_or res |> rule_dedup |> rule_simple
-    | LL_and x ->
-      let res = List.map aux x in
-      LL_and res |> rule_dedup |> rule_simple
-    | x -> x in
+    | List (op,l) ->
+      let e = List.map aux l |> boolean_of_list op in
+      f e
+    | x -> f x in
   aux exp
 
 let simplify ?(v=false) exp =
-  let bexp = bool_exp_of_php_exp exp in
+  let exp' = bool_exp_of_php_exp exp in
   if v then
     (printf "\n[+] Exp:\n%!";
-     printf "%s\n%!" @@ to_string ~z3:true bexp;
-     printf "%s\n%!" @@ to_string bexp);
-  let flat_exp = flatten bexp in
+     printf "%s\n%!" @@ to_string exp');
+  let exp'' = bur_map rule_dedup exp' in
   if v then
-    (printf "[+] Flat:\n%!";
-     printf "%s\n%!" @@ to_string flat_exp);
-  let simple = rewrite flat_exp in
-  if v then
-    (printf "[+] Simpl:\n%!";
-     printf "%s\n%!" @@ to_string simple)
+    (printf "\n[+] Exp':\n%!";
+     printf "%s\n%!" @@ to_string exp'')
 
 let check ast =
   let open Ast in
