41a8b9235f5dabec1d0f76f00dd9e726add093f2
Rijnard <rvantonder@gmail.com>
2016-06-23 23:30:18 -0400

    add test file to unit tests. update test file

--- tests/php/scheck/micro_clones.php
+++ tests/php/scheck/micro_clones.php
@@ -1,7 +1,7 @@
 <?php
 
 //Declare variables so that "Undeclared variable" check isn't triggered.
-$a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l, $m, $n, $o, $p, $q, $r, $s, $t, $u, $x, $y, $z = true;
+$a = $b = $c = $d = $e = $f = $g = $h = $i = $j = $k = $l = $m = $n = $o = $p = $q = $r = $s = $t = $u = $x = $y = $z = true;
 
 //ERROR: duplicate variable $a
 if ($a || $b || $a) {}
@@ -12,7 +12,7 @@ if ($a and $a) {}
 //ERROR: duplicate variable $a
 if ($a or $a) {}
 
-//No error due to && with higher precedence
+//No error on $a due to && with higher precedence
 if ($a and $b && $a) {}
 
 //ERROR: duplicate variable $c
@@ -24,13 +24,13 @@ if (($a and $b && $a && ($c & ($c & $c)))) {}
 //ERROR: duplicate variable $m
 if ($a && $b || $c || $m && $m) {}
 
-// No error due to operator precedence
+// No error on $m due to operator precedence
 if ($a || $b || $c && $m || $m) {}
 
 //ERROR: duplicate variable $b (checks with parentheses)
 if (($a || (($b)) || ((($b)))) || ($b)) {}
 
-//No error due to operator precedence
+//No error on $x due to operator precedence
 if ($a || $b || $c && $d || $e && $f || $x || $y && $g && $h && $i || $d || $j && $k && $l && $x && $z) {}
 
 //ERROR: duplicate variable $e
